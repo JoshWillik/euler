6160f807349129df2fcef10ea92977ccfc41344f
Josh Vanderwillik <joshwillik@gmail.com>
2016-04-09 22:18:28 -0400

    Add ability to document solutions in readme

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -3,9 +3,19 @@ import json
 with open('challenges.json', 'r') as f:
     challenges = json.load(f)
 
+with open('solutions.json', 'r') as f:
+    solutions = json.load(f)
+
 with open('README.md', 'w') as f:
-    for challenge in challenges:
-        print('✘ {id}: {title}'.format(**challenge), file=f)
-        print('', file=f)
-        print('{description}'.format(**challenge), file=f)
-        print('', file=f)
+    print('# Josh Does Project Euler\n', file=f)
+    for i, challenge in enumerate(challenges):
+        print('## ✘ {id}: {title}\n'.format(**challenge), file=f)
+        print('{description}\n'.format(**challenge), file=f)
+
+        try:
+            solved_with = solutions[str(i + 1)]['solutions']
+            if len(solved_with):
+                print('### Solutions', file=f)
+                for solution in solved_with:
+                    print('- {}'.format(solution), file=f)
+        except Exception as e: pass
